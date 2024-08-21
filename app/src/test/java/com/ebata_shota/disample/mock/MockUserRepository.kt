8a905e21ae49c424d07e0a3321c9371b0e6c2418
ebata-shota <maamaaomosiroi@gmail.com>
2024-08-21 23:52:28 +0900

    pra-3-2：UserRepositoryをInterface化

--- app/src/test/java/com/ebata_shota/disample/mock/MockUserRepository.kt
+++ app/src/test/java/com/ebata_shota/disample/mock/MockUserRepository.kt
@@ -1,23 +1,23 @@
 package com.ebata_shota.disample.mock
 
 import com.ebata_shota.disample.domain.model.User
+import com.ebata_shota.disample.infra.repository.UserRepository
 
-class MockUserRepository {
+class MockUserRepository : UserRepository {
 
     var valueGetUser: Int? = null
-
-    fun getUser(userId: Int): User? {
+    override fun getUser(userId: Int): User? {
         valueGetUser = userId
         return User(userId, "dummy")
     }
 
     var valueSaveUser: User? = null
-    fun saveUser(user: User) {
+    override fun saveUser(user: User) {
         valueSaveUser = user
     }
 
     var valueRemoveUser: Int? = null
-    fun removeUser(userId: Int) {
+    override fun removeUser(userId: Int) {
         valueRemoveUser = userId
     }
 }
