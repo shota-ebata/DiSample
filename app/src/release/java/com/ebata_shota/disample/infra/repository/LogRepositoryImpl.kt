25f45085b39f775c84a6b213f5c03dfc3c3fbe24
ebata-shota <maamaaomosiroi@gmail.com>
2024-09-11 22:54:21 +0900

    pra-6-ex2-5 LogRepositoryImplをdebugとreleaseに分ける

---
+++ app/src/release/java/com/ebata_shota/disample/infra/repository/LogRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.ebata_shota.disample.infra.repository
+
+import com.ebata_shota.disample.domain.repository.LogRepository
+import javax.inject.Inject
+
+/**
+ * Release用
+ */
+class LogRepositoryImpl
+@Inject
+constructor() : LogRepository {
+    override fun sendEventLog(message: String) {
+        // リリース版ではログを送らない
+    }
+
+    override fun sendPageLog(message: String) {
+        // リリース版ではログを送らない
+    }
+}
